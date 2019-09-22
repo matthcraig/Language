76b7c979338c42fd28073bf681ef5183fd5caf41
Matthew Craig <mgcraig78@gmail.com>
2019-09-22 15:48:01 -0400

    Add questions and edit html, declare variables in js.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $("#question form").submit(function(event) {
         var night = $("input:radio[name=night]:checked").val();
         var person = $("input:radio[name=person]:checked").val();
+        var cash = $("input:radio[name=cash]:checked").val();
     })
 })
