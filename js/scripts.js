1f15a1de5a1bb49fdae6101844035061393dfebe
Matthew Craig <mgcraig78@gmail.com>
2019-09-22 22:23:04 -0400

    Finish adding questions, add CSS properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
-    $("#question form").submit(function(event) {
+$(function() {
+    $("#questions form").submit(function(event) {
         var night = $("input:radio[name=night]:checked").val();
         var person = $("input:radio[name=person]:checked").val();
         var cash = $("input:radio[name=cash]:checked").val();
-    })
-})
+    if (night === "fruit"){
+        $("#ruby").show();
+   }
+   
+   event.preventDefault();
+    });
+});
