2f1b338e21ff3477e044f009a6af8afdaeb8babf
Matthew Craig <mgcraig78@gmail.com>
2019-09-23 21:07:35 -0400

    Add JS components and branching, add readme, add CSS properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 $(function() {
-    $("#questions form").submit(function(event) {
-        var night = $("input:radio[name=night]:checked").val();
-        var person = $("input:radio[name=person]:checked").val();
-        var cash = $("input:radio[name=cash]:checked").val();
-    if (night === "fruit"){
-        $("#ruby").show();
-   }
+  $("#questions form").submit(function(event) {
+    var night = $("input:radio[name=night]:checked").val();
+    var person = $("input:radio[name=person]:checked").val();
+    var cash = $("input:radio[name=cash]:checked").val();
+    var rather = $("input:radio[name=cash]:checked").val();
+    var car = $("input:radio[name=cash]:checked").val();
+
+      if (night === "fruit" || person === "pitcher" || cash === "happiness"){
+        $("#ruby").fadeTo("slow", 0.8);
+      } 
+      else if (night === "chess" || person === "Hawking"){
+        $("#C").fadeTo("slow", 0.8);
+      } 
+      else if (night === "beer" || person === "tasty" || cash === "dollars" ||        rather === "beach" || car === "call"){
+        $("#javascript").fadeTo("slow", 0.8);
+      } 
+      else 
+      {
+       $("#nothing").fadeTo("slow", 0.8);
+      }
    
-   event.preventDefault();
-    });
+      event.preventDefault();
+      });
 });
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Ideal Programming Language Selector
+
+#### Logical Questionnaire
+
+#### By _**Matthew Craig**_
+
+## Description
+
+_This is a page created to demonstrate branching in JavaScript._
+
+## Setup/Installation Requirements
+
+* _Simple set-up, simply load into a browser, answer the questionnaire, and find your results at the bottom of the page_
+
+
+_You will need a github account currently in order to view the page_
+
+## Known Bugs
+
+_No known bugs_
+
+## Support and contact details
+
+_Any issues - contact Matthew Craig, mgcraig78@gmail.com_
+
+## Technologies Used
+
+_This page is built using HTML, CSS, and JavaScript._
+
+### License
+
+*MIT License*
+
+Copyright (c) 2019 **_Matthew Craig_**
